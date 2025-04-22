88c4dc8b3dae69fde5aa70109cd195602d1bf755
taykim01 <taykim01@gmail.com>
2025-04-23 00:58:44 +0900

    fix: 시발

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Sylva Board",
@@ -58,6 +59,7 @@ export default function RootLayout({
       </head>
       <body className={pretendard.variable}>
         {children} <Toaster position="bottom-left" />
+        <GoogleAnalytics gaId="G-PEY6NYJ1MN" />
       </body>
     </html>
   );
