5273c12c9af2cdf2cd6fef34065e5acdfe4bee79
taykim01 <taykim01@gmail.com>
2025-04-22 23:42:23 +0900

    fix: GA 망해라

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
       </head>
       <body className={pretendard.variable}>
         {children} <Toaster position="bottom-left" />
-        <GoogleAnalytics gaId="G-W662XT6GZS" />
+        <GoogleAnalytics gaId="G-1GDM8ZBGRB" />
       </body>
     </html>
   );
