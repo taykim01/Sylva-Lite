fad7107dca7c2ea0daa4f435b287b7ee07401073
taykim01 <taykim01@gmail.com>
2025-04-23 18:42:48 +0900

    feat: redesign main page with a new card component for user interaction

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,8 +1,24 @@
-import { handleGetUser } from "@/features/auth-features";
-import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
-export default async function Page() {
-  const { error } = await handleGetUser();
-  if (error) redirect("/sign-in");
-  else redirect("/dashboard");
+export default function Page() {
+  return (
+    <div className="w-screen h-screen grid place-items-center bg-gray-50">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Welcome to Sylva!</CardTitle>
+          <CardDescription>What do you want to do?</CardDescription>
+        </CardHeader>
+        <CardFooter className="flex gap-2 justify-end">
+          <Link href="/demo">
+            <Button variant="secondary">Try out demo</Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button>Go to my board</Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
