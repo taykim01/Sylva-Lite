101c0321e02e4b43fa453ac5b01654cb5c5e6ad3
taykim01 <taykim01@gmail.com>
2025-04-23 18:50:11 +0900

    fix: update Content-Security-Policy to allow scripts from Google Tag Manager and change start_url in manifest.json to root

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline' https://use.typekit.net https://p.typekit.net",
               "font-src 'self' https://use.typekit.net https://p.typekit.net",
               "connect-src 'self' https://use.typekit.net https://p.typekit.net",
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,24 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
+import { handleGetUser } from "@/features/auth-features";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <div className="w-screen h-screen grid place-items-center bg-gray-50">
-      <Card className="w-96">
-        <CardHeader>
-          <CardTitle>Welcome to Sylva!</CardTitle>
-          <CardDescription>What do you want to do?</CardDescription>
-        </CardHeader>
-        <CardFooter className="flex gap-2 justify-end">
-          <Link href="/demo">
-            <Button variant="secondary">Try out demo</Button>
-          </Link>
-          <Link href="/dashboard">
-            <Button>Go to my board</Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+export default async function Page() {
+  const { error } = await handleGetUser();
+  if (error) redirect("/sign-in");
+  else redirect("/dashboard");
 }
