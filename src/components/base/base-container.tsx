3afd4bc9bebd4db50b4fe0332132aa70c218a78d
Tae Eun Kim <72125895+taykim01@users.noreply.github.com>
2025-06-13 11:04:00 +0000

    add todo comment

--- src/components/base/base-container.tsx
+++ src/components/base/base-container.tsx
@@ -36,7 +36,7 @@ export function BaseContainer({
   showTryButton = false,
   showSignOutButton = false,
 }: BaseContainerProps) {
-  const { settings, signOut } = useAuth();
+  const { settings, signOut } = useAuth(); // TODO: did not finish implementing the settings feature. stopped at displaying the switch's mode according to the settings in the db
   const router = useRouter();
 
   const toSignIn = async () => {
